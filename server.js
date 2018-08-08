a3ea1bc498decbf1d46528f8072a366209d9364c
rex <rhexachdheydhan@icloud.com>
2018-08-08 16:03:28 -0400

    Addition of projects page

--- server.js
+++ server.js
@@ -47,7 +47,13 @@ app.get('/', (req, res) => {
 app.get('/about',(req, res) => {
  res.render('about.hbs', {
   pageTitle: "About page",
+ });
+});
 
+app.get('/projects',(req, res) => {
+ res.render('projects.hbs', {
+  pageTitle: "Projects page",
+  welcomeMessage: "Projects page"
  });
 });
 
